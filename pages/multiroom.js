680fca49cd7044f45ae8678a3690cade176dd116
luke-meadows <lukemeadowsdev@gmail.com>
2022-05-05 14:34:42 +0100

    pages

---
+++ pages/multiroom.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default function Multiroom() {
+  return (
+    <AboutUsPage style={{ margin: '20rem 6rem' }}>
+      <h2>coming soon</h2>
+    </AboutUsPage>
+  );
+}
+
+const AboutUsPage = styled.div``;
