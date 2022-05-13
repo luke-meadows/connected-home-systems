8d71ca3d7fd4c3bdf2693a8faaa8ef936eb78dda
luke-meadows <lukemeadowsdev@gmail.com>
2022-05-13 14:16:24 +0100

    test

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,9 +1,11 @@
 import Gallery from '../components/gallery/Gallery';
 
 export default function GalleryPage() {
+  console.log('test');
   return (
     <div style={{ marginTop: '6rem', padding: '3rem 6rem 0 6rem' }}>
       <h2 style={{ marginBottom: '2rem' }}>Gallery</h2>
+
       <Gallery />
     </div>
   );
